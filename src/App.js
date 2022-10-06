efbdc824d757c46159e9ff4632b0f02dcc6d80f6
Abhay Pareek <abhaypareek170@gmail.com>
2022-10-06 15:33:32 +0530

    onClose button and Backdrop functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meal from "./Components/Meals/Meal";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = ()=>{
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = ()=>{
+    setCartIsShown(false);
+  }
   return (
     <>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meal/>
       </main>
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <>
      <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart} onClose={props.onHideCart}/>
      </header>
      <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table of tasty food.'/>
