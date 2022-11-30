0d67af84395cf814725b834a3929ea5281dab1e5
Abhay Pareek <abhaypareek170@gmail.com>
2022-11-30 20:48:58 +0530

    Update UI

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
-import HeaderCartButton from './HeaderCartButton'
-
+import React from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
-     <header className={classes.header}>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton onClick={props.onShowCart} onClose={props.onHideCart}/>
-     </header>
-     <div className={classes['main-image']}>
-        <img src={mealsImage} alt='A table of tasty food.'/>
-     </div>
-    
+        <HeaderCartButton
+          onClick={props.onShowCart}
+          onClose={props.onHideCart}
+        />
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src={mealsImage}
+          alt="A table of tasty food."
+          style={{
+            width: "110%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "rotateZ(-5deg) translateY(-4rem) translateX(-1rem)",
+          }}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
